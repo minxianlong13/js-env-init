395a3ebfe2797e837bc671489f8cabc06df68883
xmin <xmin@cvent.com>
2022-05-20 18:54:41 -0300

    Iniitial commit - project setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-env-init
 Build a JavaScript starter kit, from Pluralsight course: **Building a JavaScript Development Environment**
+  
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
